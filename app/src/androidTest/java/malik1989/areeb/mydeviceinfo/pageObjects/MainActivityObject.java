4a6cd11983a54e4f5aa29208fdf113df9ab31b28
Unknown <areeb.malik1989@gmail.com>
2018-02-24 19:31:09 +0530

    Added function areAllFragmentSetInDrawer() to test fragements list.

--- app/src/androidTest/java/malik1989/areeb/mydeviceinfo/pageObjects/MainActivityObject.java
+++ app/src/androidTest/java/malik1989/areeb/mydeviceinfo/pageObjects/MainActivityObject.java
@@ -4,7 +4,9 @@ import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.BySelector;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObject2;
 import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 
 /**
@@ -14,6 +16,7 @@ import android.support.test.uiautomator.UiSelector;
 public class MainActivityObject {
 
     private UiDevice uiDevice;
+
     private BySelector openToggleButton = By.desc("Open");
     private BySelector closeToggleButton = By.desc("Close");
 
@@ -27,5 +30,19 @@ public class MainActivityObject {
 
     public void pressCloseToggleButton() throws UiObjectNotFoundException {
         uiDevice.findObject(closeToggleButton).click();
+    }
+
+    public boolean areAllFragmentsSetInDrawer(String[] list) throws UiObjectNotFoundException {
+
+        UiScrollable scrollable = new UiScrollable(new UiSelector().scrollable(true));
+
+        for(String text:list){
+            scrollable.scrollTextIntoView(text);
+            if(uiDevice.findObject(By.text(text)) == null){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
