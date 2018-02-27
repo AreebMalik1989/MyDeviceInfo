2b8301c58a6c9236fab9a7fb0cc60b775475f3e7
Unknown <areeb.malik1989@gmail.com>
2018-02-27 22:48:55 +0530

    Added dto class DeviceProperty.java

---
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/dto/DeviceProperty.java
@@ -0,0 +1,34 @@
+package malik1989.areeb.mydeviceinfo.dto;
+
+/**
+ * Created by areeb on 27/2/18.
+ */
+
+public class DeviceProperty {
+
+    String property, value;
+
+    public DeviceProperty(){
+    }
+
+    public DeviceProperty(String property, String value){
+        this.property = property;
+        this.value = value;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
