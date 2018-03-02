e0ddc37c7ee841b5b842b825c9a3da7dfe19907d
Unknown <areeb.malik1989@gmail.com>
2018-03-02 16:21:27 +0530

    Added a dto Specs.java to store specs of device

---
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/dto/Specs.java
@@ -0,0 +1,126 @@
+package malik1989.areeb.mydeviceinfo.dto;
+
+/**
+ * Created by areeb on 2/3/18.
+ */
+
+public class Specs {
+
+    private String modelNumber;
+    private String productName;
+    private String osVersion;
+    private String processorName;
+    private String processorCore;
+    private String backCamera;
+    private String frontCamera;
+    private String ram;
+    private String rom;
+    private String screenResolution;
+    private String serialNumber;
+    private String imei1;
+    private String imei2;
+
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getOsVersion() {
+        return osVersion;
+    }
+
+    public void setOsVersion(String osVersion) {
+        this.osVersion = osVersion;
+    }
+
+    public String getBackCamera() {
+        return backCamera;
+    }
+
+    public void setBackCamera(String backCamera) {
+        this.backCamera = backCamera;
+    }
+
+    public String getFrontCamera() {
+        return frontCamera;
+    }
+
+    public void setFrontCamera(String frontCamera) {
+        this.frontCamera = frontCamera;
+    }
+
+    public String getProcessorName() {
+        return processorName;
+    }
+
+    public void setProcessorName(String processorName) {
+        this.processorName = processorName;
+    }
+
+    public String getProcessorCore() {
+        return processorCore;
+    }
+
+    public void setProcessorCore(String processorCore) {
+        this.processorCore = processorCore;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public void setRam(String ram) {
+        this.ram = ram;
+    }
+
+    public String getRom() {
+        return rom;
+    }
+
+    public void setRom(String rom) {
+        this.rom = rom;
+    }
+
+    public String getScreenResolution() {
+        return screenResolution;
+    }
+
+    public void setScreenResolution(String screenResolution) {
+        this.screenResolution = screenResolution;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getImei1() {
+        return imei1;
+    }
+
+    public void setImei1(String imei1) {
+        this.imei1 = imei1;
+    }
+
+    public String getImei2() {
+        return imei2;
+    }
+
+    public void setImei2(String imei2) {
+        this.imei2 = imei2;
+    }
+}
