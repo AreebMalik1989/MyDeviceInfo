86c0e9a085fb0fbd91c1e520b749fd0ce7f2e652
Unknown <areeb.malik1989@gmail.com>
2018-07-23 22:48:03 +0530

    Updated ObservableApk.java

--- app/src/main/java/malik1989/areeb/mydeviceinfo/observable/ObservableApk.java
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/observable/ObservableApk.java
@@ -21,91 +21,20 @@ public class ObservableApk extends Apk {
         observers.remove(apkObserver);
     }
 
-    @Override
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-        notifyPackageNameChange();
+    public void updateApkInfo(Apk apk){
+
+        setPackageName(apk.getPackageName());
+        setVersionName(apk.getVersionName());
+        setVersionCode(apk.getVersionCode());
+        setFirstInstallTime(apk.getFirstInstallTime());
+        setLastUpdateTime(apk.getLastUpdateTime());
+        setSharedUserLabel(apk.getSharedUserLabel());
+        setSharedUserId(apk.getSharedUserId());
+
+        notifyChange();
     }
 
-    public void notifyPackageNameChange() {
-        for(ApkObserver observer : observers) {
-            observer.updatePackageName(getPackageName());
-        }
-    }
-
-    @Override
-    public void setVersionName(String versionName) {
-        this.versionName = versionName;
-        notifyVersionNameChange();
-    }
-
-    public void notifyVersionNameChange() {
-        for(ApkObserver observer : observers) {
-            observer.updateVersionName(getVersionName());
-        }
-    }
-
-    @Override
-    public void setVersionCode(int versionCode) {
-        this.versionCode = versionCode;
-        notifyVersionCodeChange();
-    }
-
-    public void notifyVersionCodeChange() {
-        for(ApkObserver observer : observers) {
-            observer.updateVersionCode(getVersionCode());
-        }
-    }
-
-    @Override
-    public void setFirstInstallTime(long firstInstallTime) {
-        this.firstInstallTime = firstInstallTime;
-        notifyFirstInstallTimeChange();
-    }
-
-    public void notifyFirstInstallTimeChange() {
-        for(ApkObserver observer : observers) {
-            observer.updateFirstInstallTime(getFirstInstallTime());
-        }
-    }
-
-    @Override
-    public void setLastUpdateTime(long lastUpdateTime) {
-        this.lastUpdateTime = lastUpdateTime;
-        notifyLastUpdateTimeChange();
-    }
-
-    public void notifyLastUpdateTimeChange() {
-        for(ApkObserver observer : observers) {
-            observer.updateLastUpdateTime(getLastUpdateTime());
-        }
-    }
-
-    @Override
-    public void setSharedUserLabel(int sharedUserLabel) {
-        this.sharedUserLabel = sharedUserLabel;
-        notifySharedUserLabelChange();
-    }
-
-    public void notifySharedUserLabelChange() {
-        for(ApkObserver observer : observers) {
-            observer.updateSharedUserLabel(getSharedUserLabel());
-        }
-    }
-
-    @Override
-    public void setSharedUserId(String sharedUserId) {
-        this.sharedUserId = sharedUserId;
-        notifySharedUserIdChange();
-    }
-
-    public void notifySharedUserIdChange() {
-        for(ApkObserver observer : observers) {
-            observer.updateSharedUserId(getSharedUserId());
-        }
-    }
-
-    public void notifyChange() {
+    private void notifyChange() {
         for(ApkObserver observer : observers) {
             observer.update(this);
         }
