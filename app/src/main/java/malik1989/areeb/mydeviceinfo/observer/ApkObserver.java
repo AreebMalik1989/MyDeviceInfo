47c6cada8c246df7b1a514f18288b0d61e6c8a93
Unknown <areeb.malik1989@gmail.com>
2018-07-23 22:48:34 +0530

    Updated ApkObserver.java

--- app/src/main/java/malik1989/areeb/mydeviceinfo/observer/ApkObserver.java
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/observer/ApkObserver.java
@@ -1,5 +1,6 @@
 package malik1989.areeb.mydeviceinfo.observer;
 
+import malik1989.areeb.mydeviceinfo.model.Apk;
 import malik1989.areeb.mydeviceinfo.observable.ObservableApk;
 
 /**
@@ -8,12 +9,5 @@ import malik1989.areeb.mydeviceinfo.observable.ObservableApk;
 
 public interface ApkObserver {
 
-    void updatePackageName(String packageName);
-    void updateVersionName(String versionName);
-    void updateVersionCode(int versionCode);
-    void updateFirstInstallTime(long firstInstallTime);
-    void updateLastUpdateTime(long lastUpdatedTime);
-    void updateSharedUserLabel(int sharedUserLabel);
-    void updateSharedUserId(String sharedUserId);
-    void update(ObservableApk observableApk);
+    void update(Apk apk);
 }
