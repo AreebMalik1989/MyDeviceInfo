5a061c9ad899730346c2bdefea1dc533fbdd5342
Unknown <areeb.malik1989@gmail.com>
2018-07-24 21:57:23 +0530

    Added BasicObserver.java

---
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/observer/BasicObserver.java
@@ -0,0 +1,12 @@
+package malik1989.areeb.mydeviceinfo.observer;
+
+import malik1989.areeb.mydeviceinfo.model.Basic;
+
+/**
+ * Created by areeb on 24/7/18.
+ */
+
+public interface BasicObserver {
+
+    void update(Basic basic);
+}
