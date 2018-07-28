b1f0b29fe7ab097688727a86a624b61b5297059c
Unknown <areeb.malik1989@gmail.com>
2018-07-28 11:21:51 +0530

    Added network and wifi in DataExchange

--- app/src/main/java/malik1989/areeb/mydeviceinfo/exchange/DataExchange.java
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/exchange/DataExchange.java
@@ -1,14 +1,10 @@
 package malik1989.areeb.mydeviceinfo.exchange;
 
-import malik1989.areeb.mydeviceinfo.model.Apk;
-import malik1989.areeb.mydeviceinfo.model.Basic;
-import malik1989.areeb.mydeviceinfo.model.Battery;
-import malik1989.areeb.mydeviceinfo.model.Processor;
-import malik1989.areeb.mydeviceinfo.model.Telephony;
-import malik1989.areeb.mydeviceinfo.model.Wifi;
 import malik1989.areeb.mydeviceinfo.observable.ObservableApk;
 import malik1989.areeb.mydeviceinfo.observable.ObservableBasic;
 import malik1989.areeb.mydeviceinfo.observable.ObservableBattery;
+import malik1989.areeb.mydeviceinfo.observable.ObservableNetwork;
+import malik1989.areeb.mydeviceinfo.observable.ObservableWifi;
 
 /**
  * Created by areeb on 23/7/18.
@@ -19,5 +15,7 @@ public interface DataExchange {
     ObservableApk apk = new ObservableApk();
     ObservableBasic basic = new ObservableBasic();
     ObservableBattery battery = new ObservableBattery();
+    ObservableNetwork network = new ObservableNetwork();
+    ObservableWifi wifi = new ObservableWifi();
 
 }
