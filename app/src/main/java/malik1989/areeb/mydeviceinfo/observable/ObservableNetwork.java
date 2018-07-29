2b5a88a6e11ede49123da3d7de8a9f293dcc1c4e
Unknown <areeb.malik1989@gmail.com>
2018-07-29 11:20:52 +0530

    Added ObservableNetwork.java

---
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/observable/ObservableNetwork.java
@@ -0,0 +1,61 @@
+package malik1989.areeb.mydeviceinfo.observable;
+
+import java.util.ArrayList;
+
+import malik1989.areeb.mydeviceinfo.model.Network;
+import malik1989.areeb.mydeviceinfo.observer.NetworkObserver;
+
+/**
+ * Created by areeb on 27/7/18.
+ */
+
+public class ObservableNetwork extends Network {
+
+    private static ArrayList<NetworkObserver> observers = new ArrayList<>();
+
+    public void addObserver(NetworkObserver networkObserver){
+        observers.add(networkObserver);
+    }
+
+    public void removeObserver(NetworkObserver networkObserver){
+        observers.remove(networkObserver);
+    }
+
+    public void updateNetworkInfo(Network network){
+        setDeviceSoftwareVersion(network.getDeviceSoftwareVersion());
+        setDeviceId(network.getDeviceId());
+        setNeighboringCellInfo(network.getNeighboringCellInfo());
+        setCellLocation(network.getCellLocation());
+        setPhoneType(network.getPhoneType());
+        setNetworkOperatorName(network.getNetworkOperatorName());
+        setNetworkOperator(network.getNetworkOperator());
+        setNetworkRoaming(network.isNetworkRoaming());
+        setNetworkCountryIso(network.getNetworkCountryIso());
+        setNetworkType(network.getNetworkType());
+        setHasIccCard(network.isHasIccCard());
+        setSimState(network.getSimState());
+        setSimOperator(network.getSimOperator());
+        setSimOperatorName(network.getSimOperatorName());
+        setSimCountryIso(network.getSimCountryIso());
+        setSimSerialNumber(network.getSimSerialNumber());
+        setSubscriberId(network.getSubscriberId());
+        setGroupIdLevel1(network.getGroupIdLevel1());
+        setLine1Number(network.getLine1Number());
+        setVoiceMailNumber(network.getVoiceMailNumber());
+        setVoiceMailAlphaTag(network.getVoiceMailAlphaTag());
+        setCallState(network.getCallState());
+        setDataActivity(network.getDataActivity());
+        setDataState(network.getDataState());
+        setAllCellInfo(network.getAllCellInfo());
+        setMmsUserAgent(network.getMmsUserAgent());
+        setMmsUAProfUrl(network.getMmsUAProfUrl());
+
+        notifyChange();
+    }
+
+    private void notifyChange() {
+        for(NetworkObserver observer : observers) {
+            observer.update(this);
+        }
+    }
+}
