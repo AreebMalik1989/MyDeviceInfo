592f8a5b61dfbe946e5480aac64c66be79392cae
Unknown <areeb.malik1989@gmail.com>
2018-07-31 07:35:27 +0530

    Updated ApkObserver.java

--- app/src/main/java/malik1989/areeb/mydeviceinfo/observer/ApkObserver.java
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/observer/ApkObserver.java
@@ -1,7 +1,6 @@
 package malik1989.areeb.mydeviceinfo.observer;
 
 import malik1989.areeb.mydeviceinfo.model.Apk;
-import malik1989.areeb.mydeviceinfo.observable.ObservableApk;
 
 /**
  * Created by areeb on 23/7/18.
