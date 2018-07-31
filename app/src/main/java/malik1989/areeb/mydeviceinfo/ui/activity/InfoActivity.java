8c37f3e0877d48e91f74a993ac9a3e53c932f168
Unknown <areeb.malik1989@gmail.com>
2018-07-31 07:34:20 +0530

    Added InfoActivity.java

---
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/ui/activity/InfoActivity.java
@@ -0,0 +1,19 @@
+package malik1989.areeb.mydeviceinfo.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import malik1989.areeb.mydeviceinfo.R;
+
+/**
+ * Created by areeb on 27/7/18.
+ */
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+}
