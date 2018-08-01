c8727e3c439788efb8749a7d78947dbdf041e77b
Unknown <areeb.malik1989@gmail.com>
2018-08-01 07:31:38 +0530

    Added MainActivityObjectTest

---
+++ app/src/androidTest/java/malik1989/areeb/mydeviceinfo/src/test/pageObjectTest/mainActivityObjectTest.java
@@ -0,0 +1,8 @@
+package malik1989.areeb.mydeviceinfo.src.test.pageObjectTest;
+
+/**
+ * Created by areeb on 29/7/18.
+ */
+
+public class mainActivityObjectTest {
+}
--- app/src/main/java/malik1989/areeb/mydeviceinfo/Temp.java
+++
@@ -1,8 +0,0 @@
-package malik1989.areeb.mydeviceinfo;
-
-/**
- * Created by areeb on 23/7/18.
- */
-
-public class Temp {
-}
