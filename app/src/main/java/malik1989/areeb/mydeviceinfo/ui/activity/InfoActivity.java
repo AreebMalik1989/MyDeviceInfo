7b6711b5ab51171a7d70ab739085192872471e54
Unknown <areeb.malik1989@gmail.com>
2018-08-02 08:05:58 +0530

    Updated InfoActivity.java to show details of app

--- app/src/main/java/malik1989/areeb/mydeviceinfo/ui/activity/InfoActivity.java
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/ui/activity/InfoActivity.java
@@ -2,8 +2,11 @@ package malik1989.areeb.mydeviceinfo.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import malik1989.areeb.mydeviceinfo.R;
+import malik1989.areeb.mydeviceinfo.collector.ApkInfoCollector;
+import malik1989.areeb.mydeviceinfo.exchange.DataExchange;
 
 /**
  * Created by areeb on 27/7/18.
@@ -11,9 +14,18 @@ import malik1989.areeb.mydeviceinfo.R;
 
 public class InfoActivity extends AppCompatActivity {
 
+    TextView version, versionCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        version = findViewById(R.id.info_version);
+        versionCode = findViewById(R.id.info_version_code);
+
+        DataExchange.apk.updateApkInfo(ApkInfoCollector.collect());
+        version.setText(DataExchange.apk.getVersionName());
+        versionCode.setText(String.valueOf(DataExchange.apk.getVersionCode()));
     }
 }
