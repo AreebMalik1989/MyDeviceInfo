27100be8e2d0f8654c99ca38a657471b680089ba
Unknown <areeb.malik1989@gmail.com>
2018-08-09 20:11:45 +0530

    Updated InfoActivity.java

--- app/src/main/java/malik1989/areeb/mydeviceinfo/ui/activity/InfoActivity.java
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/ui/activity/InfoActivity.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import github.nisrulz.easydeviceinfo.base.EasyAppMod;
 import malik1989.areeb.mydeviceinfo.R;
-import malik1989.areeb.mydeviceinfo.collector.ApkInfoCollector;
 import malik1989.areeb.mydeviceinfo.exchange.DataExchange;
 
 /**
@@ -18,14 +18,16 @@ public class InfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        EasyAppMod appMod = new EasyAppMod(this);
 
         version = findViewById(R.id.info_version);
         versionCode = findViewById(R.id.info_version_code);
 
-        DataExchange.apk.updateApkInfo(ApkInfoCollector.collect());
-        version.setText(DataExchange.apk.getVersionName());
-        versionCode.setText(String.valueOf(DataExchange.apk.getVersionCode()));
+        version.setText(appMod.getAppVersion());
+        versionCode.setText(String.valueOf(appMod.getAppVersionCode()));
     }
 }
