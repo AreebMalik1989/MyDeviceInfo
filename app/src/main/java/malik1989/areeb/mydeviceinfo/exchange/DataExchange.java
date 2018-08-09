45a332bde0ca325644dbd43cb1544ae023518ff5
Unknown <areeb.malik1989@gmail.com>
2018-08-09 20:10:13 +0530

    Removed ObservableApk and renamed ObservableBasic

--- app/src/main/java/malik1989/areeb/mydeviceinfo/exchange/DataExchange.java
+++ app/src/main/java/malik1989/areeb/mydeviceinfo/exchange/DataExchange.java
@@ -1,9 +1,10 @@
 package malik1989.areeb.mydeviceinfo.exchange;
 
-import malik1989.areeb.mydeviceinfo.observable.ObservableApk;
-import malik1989.areeb.mydeviceinfo.observable.ObservableBasic;
+import malik1989.areeb.mydeviceinfo.observable.ObservableDevice;
 import malik1989.areeb.mydeviceinfo.observable.ObservableBattery;
 import malik1989.areeb.mydeviceinfo.observable.ObservableNetwork;
+import malik1989.areeb.mydeviceinfo.observable.ObservableSensor;
+import malik1989.areeb.mydeviceinfo.observable.ObservableSim;
 import malik1989.areeb.mydeviceinfo.observable.ObservableWifi;
 
 /**
@@ -12,10 +13,10 @@ import malik1989.areeb.mydeviceinfo.observable.ObservableWifi;
 
 public interface DataExchange {
 
-    ObservableApk apk = new ObservableApk();
-    ObservableBasic basic = new ObservableBasic();
+    ObservableDevice device = new ObservableDevice();
     ObservableBattery battery = new ObservableBattery();
     ObservableNetwork network = new ObservableNetwork();
     ObservableWifi wifi = new ObservableWifi();
-
+    ObservableSim sim = new ObservableSim();
+    ObservableSensor sensor = new ObservableSensor();
 }
